3778fb2a9c60a9e8a13de39f20e60458787b0cd2
hwon22 <s2018s06@e-mirim.hs.kr>
2020-05-06 15:35:52 +0900

    monthseason 실습 소스

--- monthseason/Program.cs
+++ monthseason/Program.cs
@@ -10,6 +10,63 @@ namespace monthseason
     {
         static void Main(string[] args)
         {
+            Console.Write("월 입력: ");
+            String month = Console.ReadLine();
+            if (month.Contains("3"))
+            {
+                Console.WriteLine("봄입니다.");
+            }
+            else if (month.Contains("4"))
+            {
+                Console.WriteLine("봄입니다.");
+            } else if (month.Contains("5"))
+            {
+                Console.WriteLine("봄입니다.");
+            }
+
+            else if (month.Contains("6"))
+            {
+                Console.WriteLine("여름입니다.");
+            }
+            else if (month.Contains("7"))
+            {
+                Console.WriteLine("여름입니다.");
+            }
+            else if (month.Contains("8"))
+            {
+                Console.WriteLine("여름입니다.");
+            }
+
+            else if (month.Contains("9"))
+            {
+                Console.WriteLine("가을입니다.");
+            }
+            else if (month.Contains("10"))
+            {
+                Console.WriteLine("가을입니다.");
+            }
+            else if (month.Contains("11"))
+            {
+                Console.WriteLine("가을입니다.");
+            }
+
+            else if (month.Contains("12"))
+            {
+                Console.WriteLine("겨울입니다.");
+            }
+            else if (month.Contains("1"))
+            {
+                Console.WriteLine("겨울입니다.");
+            }
+            else if (month.Contains("2"))
+            {
+                Console.WriteLine("겨울입니다.");
+            }
+            else
+            {
+                Console.WriteLine("잘못 입력했다.");
+            }
+
         }
     }
 }
